3907a30f771ca143b8a7ee5e8cf14d878aeea494
bryan <bryanlbottorff@gmail.com>
2022-04-23 11:54:03 -0400

    complete component templates for cart footer navber layout and dynamic product pages

--- all_heart_bakery/components/Footer.jsx
+++ all_heart_bakery/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>
+        2022 All Heart Bakery | All Rights Reserved
+      </p>
+      <p className='icons'>
+        <AiFillFacebook/>
+        <AiFillInstagram/>
+      </p>
+    </div>
   )
 }
 
